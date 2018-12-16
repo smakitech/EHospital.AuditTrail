04e43872e88a401f8e7db9ac57636ae729a8cf55
smoukiDev <smakfactory@gmail.com>
2018-12-17 00:05:47 +0200

    Added and set swagger.

--- EHospital.AuditTrail.WebAPI/Startup.cs
+++ EHospital.AuditTrail.WebAPI/Startup.cs
@@ -10,11 +10,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace EHospital.AuditTrail.WebAPI
 {
     public class Startup
     {
+        ///* Swagger setting
+        private const string VERSION = "v.1.0.0";
+        private const string API_NAME = "EHospital.AuditTrail";
+        //*/
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +32,21 @@ namespace EHospital.AuditTrail.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            ///* Swagger Setting
+            Info info = new Info
+            {
+                Version = VERSION,
+                Title = API_NAME,
+                Description = "Micro-service provides REST request for logging operations history"
+                            + "about operations performing in other EHospital micro-services.",
+                Contact = new Contact()
+                {
+                    Name = "Serhii Maksymchuk",
+                    Email = "smakdealcase@gmail.com"
+                }
+            };
+            services.AddSwaggerGen(c => { c.SwaggerDoc(VERSION, info); });
+            //*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +63,14 @@ namespace EHospital.AuditTrail.WebAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            ///* Swagger Setting
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"../swagger/{VERSION}/swagger.json", API_NAME);
+            });
+            //*/
         }
     }
 }
